89aeb25a4efa7a6d3c7022a158ae938f83e72da7
Arthur Fritz <arthurfritzz@gmail.com>
2018-04-30 13:59:03 -0300

    Learning leason two, three - Alura

---
+++ ocajp-alura/src/i/leason/three/HelloWorld.java
@@ -0,0 +1,34 @@
+package i.leason.three;
+
+public class HelloWorld {
+
+    //javac HelloWorld.java
+    //java i.leadon.three.HelloWorld
+    //java -Dpropriedade=Valor HelloWorld
+
+    //change classpath use folder location, separate unix (:) and windows (;)
+    //java -cp folder i.leadon.three.HelloWorld
+    //java -classpath folder i.leadon.three.HelloWorld
+    public static void main (String[] args){ //String... args, String args[]
+        System.out.println("Rodando o programa");
+        System.out.println(args.length);
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+
+    //Versão do java ao compilar
+    //javac MinhaClasse.java -source 1.3
+
+    //Parâmetro como varargs
+    //public static void main (String... args) {}
+
+    //A ordem dos modificadores não importa
+    //static public void main(String[] args) {}
+
+    //O nome do parâmetro não importa
+    //public static void main (String... argumentos){}
+
+    //Também é uma definição válida de array
+    //public static void main (String args[]) {}
+}
---
+++ ocajp-alura/src/i/leason/two/StructureClass.java
@@ -0,0 +1,50 @@
+//First and optional package 1..0
+package i.leason.two;
+import java.util.Date;
+
+//0..n
+interface A {
+    int TAMANHO = 5; //default variable is public static final do not alter value
+    void autentica(String name, String password); //default method is public
+}
+
+//0..n
+class B implements A {
+    int B;
+    B(){}
+    void B(){ }
+
+    @Override
+    public void autentica(String name, String password) {
+
+    }
+}
+
+// public 0..1 but of class name same name archive
+public class /* name of classe */ StructureClass {
+
+    static int totalDePessoas = 0;
+
+    String name;
+    StructureClass(String name){
+        if(name == null){
+            this.name = "Arthur";
+            return; //somente return vazio
+        }
+        this.name = name;
+    }
+
+    /**
+     * Javadoc, this method return full name;
+     * @return - full name
+     */
+    public String getName() {
+        String middleName = " Fritz";
+        return name + middleName;
+    }
+
+    public static void main(String[] args){
+        StructureClass structureClass = new StructureClass(null);
+        System.out.println(structureClass.getName());
+    }
+}
